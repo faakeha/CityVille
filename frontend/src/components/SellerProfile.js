eee9943e24fa89b75d5af72bb5a92c8f5070aaa3
S-Wajiha-Batool <w.batool_19684@khi.iba.edu.pk>
2022-06-01 17:18:51 +0500

    create appointment

--- frontend/src/components/SellerProfile.js
+++ frontend/src/components/SellerProfile.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect, useContext } from "react";
 import { Card } from "react-bootstrap";
 import "./SellerProfile.css";
 import { GlobalState } from "../GlobalState";
-import {Link,  useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Button } from "react-bootstrap";
+
 
 
 const SellerProfile = () => {
@@ -10,7 +12,7 @@ const SellerProfile = () => {
 	const uid = params.id;
 	console.log(uid)
 	const userid = uid ? uid : -1
-	
+
 
 
 	//const [response, setResponse] = useState();
@@ -22,51 +24,90 @@ const SellerProfile = () => {
 	const [listing, setListing] = useState([]);
 	var id = ''
 
-	
+
 
 	const get_user = async (event) => {
 		console.log("in login method");
 		const token = localStorage["userToken"];
+		const fromid = localStorage["user_id"];
+var today = new Date(); 
 
-		if(userid !== -1){
+		if (userid !== -1) {
 			console.log('in userid if')
 			id = userid
 			console.log(userid, 'id')
-			const data = await fetch(`http://localhost:3001/CityVille/users?id=${id}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				token: `Bearer ${token}`,
-			},
-		});
+			const result = fetch(`http://localhost:3001/CityVille/users?id=${id}`, { 
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					token: `Bearer ${token}`,
+				},
+			 })
+				.then(response => response.json()) // pass the data as promise to next then block
+				.then(data => {
+					setName(data.first_name + " " + data.last_name);
+					setID(data._id);
 
-		const res = await data.json();
-		console.log("eeeeeeeeeeeeeeeeeeeeee", res);
-		
-		setName(res.first_name + " " + res.last_name);
-		setID(res._id);
+					console.log(data, '\n');
+
+					return fetch(`http://localhost:3001/CityVille/createAppointment`, {
+						method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					token: `Bearer ${token}`,
+				},
+				body:JSON.stringify({
+					to_user_id: userid,
+					from_user_id: fromid,
+					date: today.getDate()
+
+				})
+					}); // make a 2nd request and return a promise
+				})
+				.then(response => response.json())
+				.catch(err => {
+					console.error('Request failed', err)
+				})
+
+			// I'm using the result const to show that you can continue to extend the chain from the returned promise
+			result.then(r => {
+				console.log(r); // 2nd request result first_stage property
+			});
+
+			// 	const data = await fetch(`http://localhost:3001/CityVille/users?id=${id}`, {
+			// 	method: "GET",
+			// 	headers: {
+			// 		"Content-Type": "application/json",
+			// 		token: `Bearer ${token}`,
+			// 	},
+			// });
+
+			// const res = await data.json();
+			// console.log("eeeeeeeeeeeeeeeeeeeeee", res);
+
 			
+
 		}
-		else{
+		else {
 			console.log('in userid else')
 			id = localStorage["user_id"];
 			const data = await fetch(`http://localhost:3001/CityVille/user/${id}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				token: `Bearer ${token}`,
-			},
-		});
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					token: `Bearer ${token}`,
+				},
+			});
 
-		const res = await data.json();
-		console.log("eeeeeeeeeeeeeeeeeeeeee", res);
-		
-		setName(res[0].first_name + " " + res[0].last_name);
-		setID(res[0]._id);
+			const res = await data.json();
+			console.log("eeeeeeeeeeeeeeeeeeeeee", res);
+
+			setName(res[0].first_name + " " + res[0].last_name);
+			setID(res[0]._id);
 		}
 
 		//event.preventDefault()
-		
+
 	};
 
 	console.log("lalal", name);
@@ -103,7 +144,17 @@ const SellerProfile = () => {
 						src="https://res.cloudinary.com/dbmknff2i/image/upload/v1653748300/users/Image_t0wi4m.png"
 						alt="Tesla"
 					/>
+
+					<Link to="/CustomerAppointments">
+
+						<Button style={{ width: "250px" }}>
+							Request an appointment
+						</Button>
+
+					</Link>
 				</div>
+
+
 
 				<div className="card-wrapper">
 					{
@@ -128,7 +179,7 @@ const SellerProfile = () => {
 					</Card>
 				</div>
 			</div>
-			
+
 
 			<div className="bottom-sp-bar">
 				{listing.map((service) => (
@@ -144,7 +195,7 @@ const SellerProfile = () => {
 							alt="user"
 						></img>
 						<div>
-							<p ><Link to={`/Individual_Listing/${service._id}/${service.user_id}`} style={{textDecoration: "none", color:"black"}}>{service.service_name}</Link></p>
+							<p ><Link to={`/Individual_Listing/${service._id}/${service.user_id}`} style={{ textDecoration: "none", color: "black" }}>{service.service_name}</Link></p>
 
 							<br></br>
 						</div>
